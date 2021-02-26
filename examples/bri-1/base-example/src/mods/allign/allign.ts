bf7c92dfec1eeae79911f349d5ac46b01a616b0d
Hamza Tokuchi <h@mza.jp>
2021-02-26 10:49:45 +0100

    Allignment rework + util for generating unique subsets

--- examples/bri-1/base-example/src/mods/allign/allign.ts
+++ examples/bri-1/base-example/src/mods/allign/allign.ts
@@ -1,4 +1,13 @@
-import { Structure, FileStructure, SupplierType } from "../types";
+import {
+  Structure,
+  FileStructure,
+  SupplierType,
+  OverlapContainer,
+  SupplierPreferenceData,
+  ReqContent,
+} from "../types";
+import { combineArrays } from "../../utils/utils";
+
 // 1. For each supplier category:
 // Open folder of received information
 // Store information of supplier in data object.
@@ -44,8 +53,17 @@ export const partitionSuppliers = (suppliers: FileStructure[]): Structure => {
 // If not, do earlyc+1 and check again untill earlyc<=(latest day of timewindow-tasklength)
 // If a overlappig day is found: eachCat = True
 // Else: Print "no overlapping day could be found" -> abort / restart process with extended timewindow
-export const findOverlappingDay = (sortedSupp: Structure) => {
-  let sets: { [key: number]: { [key: string]: string[] } } = {};
+
+export const findOverlappingDay = (
+  sortedSupp: Structure
+): [OverlapContainer, SupplierPreferenceData] => {
+  let sets: OverlapContainer = {};
+  let preferenceSets: SupplierPreferenceData = {};
+
+  const minNum = Object.values(sortedSupp).reduce(
+    (pVal: any, cVal: any) => (cVal.length > 0 ? pVal + 1 : pVal + 0),
+    0
+  );
 
   // Time window states that there must be a job done with length N between time_window 0 and 1
   for (const [category, supplierSet] of Object.entries(sortedSupp)) {
@@ -77,6 +95,17 @@ export const findOverlappingDay = (sortedSupp: Structure) => {
     for (const [_, supplierSet] of Object.entries(sortedSupp)) {
       for (const supplier of supplierSet) {
         if (supplier._content.supplierAvailability[parseInt(key) - 1]) {
+          preferenceSets = {
+            ...{
+              ...preferenceSets,
+            },
+            [supplier._content.supplierId]: {
+              supplierCost: supplier._content.supplierCost,
+              supplierReputation: supplier._content.supplierReputation,
+              supplierType: supplier._type,
+            },
+          };
+
           switch (supplier._type) {
             case SupplierType.PORT: {
               sets[parseInt(key)]["Ports"].push(supplier._content.supplierId);
@@ -101,18 +130,139 @@ export const findOverlappingDay = (sortedSupp: Structure) => {
       }
     }
   }
+
+  // Time to go through the days and see if we actually have sets of suppliers of the correct size.
+  // This is where it might become apparant that we dont have enough suppliers for a given task.
+  const viableResultFilter = dayReducer(sets, minNum);
+  const finalResult =
+    Object.keys(viableResultFilter).length > 0
+      ? {
+          [Object.keys(viableResultFilter)[0]]:
+            viableResultFilter[parseInt(Object.keys(viableResultFilter)[0])],
+        }
+      : {};
+
+  return [finalResult, preferenceSets];
+};
+
+const dayReducer = (
+  totalSet: OverlapContainer,
+  min: number
+): OverlapContainer => {
+  // First loop through all days
+  let reducedTotalSet: OverlapContainer = {};
+
+  for (const [day, suppliers] of Object.entries(totalSet)) {
+    if (Object.values(suppliers).length === min) {
+      reducedTotalSet = {
+        ...{
+          ...reducedTotalSet,
+        },
+        [day]: suppliers,
+      };
+    }
+  }
+
+  return reducedTotalSet;
 };
 
 // 3. For the found earlyC, find the matching supplierIDs
 // Create all possible, unique subsets
-export const createUniqueSets = () => {};
+export const createUniqueSets = (coreSet: OverlapContainer): string[][] => {
+  // Make sure we are working with a single entry
+  if (Object.keys(coreSet).length === 0 || Object.keys(coreSet).length > 1)
+    return [];
+
+  // Grab the current day we're working with now
+  const currentDay = Object.keys(coreSet)[0];
+
+  // Grab all the suppliers listed under currentDay
+  const cSuppliers = coreSet[parseInt(currentDay)];
+
+  // If we reach this point it means we need to start generating all
+  // unique subsets for all our suppliers.
+  let tempContainer: string[][] = [];
+
+  for (let supplierArray of Object.values(cSuppliers)) {
+    tempContainer.push(supplierArray);
+  }
+
+  // Time to generate the unique sets of suppliers.
+  let uniqueSets: string[][] = combineArrays(tempContainer);
+
+  return uniqueSets;
+};
 
 // 4. For the matching supplierIDs, find price and rating
 // Calculate for each subset:
 // 	Total price based on tasklen
 //  Amount of tech and spares.
 //  Average rating.
-export const formatUniqueSets = () => {};
+export const formatUniqueSets = (
+  uniqueSetIds: string[][],
+  metaDict: SupplierPreferenceData,
+  mjReqs: ReqContent
+): any => {
+  let optimalContainer: {
+    supplierSet: string[];
+    totalPrice: number;
+    averageRating: number;
+  }[] = [];
+
+  console.log(`\n------------------`);
+  console.log(`${JSON.stringify(mjReqs, undefined, 2)}`);
+  console.log(`------------------`);
+
+  for (const supSet of uniqueSetIds) {
+    // Each supSet contains multiple IDS of our needed suppliers
+    // Loop through these too and calculate:
+    // Total price based on tasklength
+    // Average rating
+    // Tech  -- tasklen*#oftech*8*price
+    // Spares -- price * unit
+    // Vesel -- price*unit*tasklen
+    // Port -- price
+    let setPrice: number = 0;
+    let setRep: number = 0;
+
+    supSet.map((id: any) => {
+      let currentMeta = metaDict[id] || [];
+      setRep += currentMeta.supplierReputation;
+      switch (currentMeta.supplierType) {
+        // Some calculations may contain a (n * 1) operation.
+        // This is simply a placeholder for the supplier's unit.
+        case SupplierType.PORT: {
+          setPrice += currentMeta.supplierCost;
+          break;
+        }
+        case SupplierType.SPARE: {
+          setPrice += currentMeta.supplierCost * 1;
+          break;
+        }
+        case SupplierType.TECHNICIAN: {
+          setPrice +=
+            currentMeta.supplierCost * 8 * mjReqs.tech * mjReqs.taskLength;
+          break;
+        }
+        case SupplierType.VESSEL: {
+          setPrice += currentMeta.supplierCost * 1 * mjReqs.taskLength;
+          break;
+        }
+      }
+    });
+
+    let avgdReputation = setRep / supSet.length;
+    optimalContainer.push({
+      supplierSet: supSet,
+      totalPrice: setPrice,
+      averageRating: avgdReputation,
+    });
+  }
+
+  // @TODO::HAMZA Create a new function here which returns either the cheapest or best reputable
+  // subset. Discuss with Yang how to implement this.
+  return optimalContainer[0];
+};
 
 // 5. Return earlyC/startday, cheapestset and determine bestset
 //For example: Cheapest subset: [[id:'Port3',amount:1,price:12],[id:'Spares2',amount:1,price:12],[id:'Techs21', amount:12, price: 100], [id:'Vessel25', amount:1, price:1120], 531400, 4.75]]
--- examples/bri-1/base-example/src/utils/utils.ts
+++ examples/bri-1/base-example/src/utils/utils.ts
@@ -28,3 +28,83 @@ export let fileReader = async (
   }
   return Promise.reject("");
 };
+
+// https://stackoverflow.com/a/51470002/9911187
+export const combineArrays = (array_of_arrays: any): any => {
+  // First, handle some degenerate cases...
+
+  if (!array_of_arrays) {
+    // Or maybe we should toss an exception...?
+    return [];
+  }
+
+  if (!Array.isArray(array_of_arrays)) {
+    // Or maybe we should toss an exception...?
+    return [];
+  }
+
+  if (array_of_arrays.length == 0) {
+    return [];
+  }
+
+  for (let i = 0; i < array_of_arrays.length; i++) {
+    if (!Array.isArray(array_of_arrays[i]) || array_of_arrays[i].length == 0) {
+      // If any of the arrays in array_of_arrays are not arrays or zero-length, return an empty array...
+      return [];
+    }
+  }
+
+  // Done with degenerate cases...
+
+  // Start "odometer" with a 0 for each array in array_of_arrays.
+  let odometer = new Array(array_of_arrays.length).fill(0);
+
+  let output: any = [];
+
+  let newCombination = formCombination(odometer, array_of_arrays);
+
+  output.push(newCombination);
+
+  while (odometer_increment(odometer, array_of_arrays)) {
+    newCombination = formCombination(odometer, array_of_arrays);
+    output.push(newCombination);
+  }
+
+  return output;
+}; /* combineArrays() */
+
+// Translate "odometer" to combinations from array_of_arrays
+const formCombination = (odometer: any, array_of_arrays: any): any => {
+  return odometer.reduce(
+    (accumulator: any, odometer_value: any, odometer_index: any) => {
+      return [...accumulator, array_of_arrays[odometer_index][odometer_value]];
+    },
+    []
+  );
+}; /* formCombination() */
+
+const odometer_increment = (odometer: any, array_of_arrays: any): any => {
+  for (
+    let i_odometer_digit = odometer.length - 1;
+    i_odometer_digit >= 0;
+    i_odometer_digit--
+  ) {
+    let maxee = array_of_arrays[i_odometer_digit].length - 1;
+
+    if (odometer[i_odometer_digit] + 1 <= maxee) {
+      // increment, and you're done...
+      odometer[i_odometer_digit]++;
+      return true;
+    } else {
+      if (i_odometer_digit - 1 < 0) {
+        // No more digits left to increment, end of the line...
+        return false;
+      } else {
+        // Can't increment this digit, cycle it to zero and continue
+        // the loop to go over to the next digit...
+        odometer[i_odometer_digit] = 0;
+        continue;
+      }
+    }
+  } /* for( let odometer_digit = odometer.length-1; odometer_digit >=0; odometer_digit-- ) */
+}; /* odometer_increment() */
